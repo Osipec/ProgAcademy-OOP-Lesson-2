ef6e7f5395264ff263e09c981c542c6670952fd8
Osip <osypenko.k@gmail.com>
2022-12-13 13:59:23 +0200

    Added logging to program

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+filehandler = logging.FileHandler(__name__)
+filehandler.setLevel(logging.INFO)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+filehandler.setFormatter(file_formatter)
+consolehandler = logging.StreamHandler()
+consolehandler.setLevel(logging.WARNING)
+cons_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+consolehandler.setFormatter(cons_formatter)
+
+logger.addHandler(filehandler)
+logger.addHandler(consolehandler)
 
 class Person:
     def __init__(self, name: str, surname: str):
@@ -26,15 +40,24 @@ class Group():
         self.__group = []
 
     def add_visitor(self, student: Student):
+        """
+        This method is called to add student to a group. Student should be an instance of "Student" Class
+        Note the exceptions raised in this method
+        """
         if not isinstance(student, Student):
             raise TypeError('Student must be instance of class "Student"')
         if len(self.__group) >= self.max_stud:
             raise MaxStudError("Maximum of students reached", len(self.__group))
         if student in self.__group:
+            logger.warning(StudInGroupError("Student already in group", student))
             raise StudInGroupError("Student already in group", student)
         self.__group.append(student)
+        logger.info('Student added')
 
     def remove_visitor(self, student: Student):
+        """
+        This method is used to remove stydent from the group.Student should be an instance of "Student" Class
+        """
         if student in self.__group:
             self.__group.remove(student)
 
@@ -50,6 +73,11 @@ class Group():
 
 
 class MaxStudError(Exception):
+    """
+        This exception is raised when you try to add student to a group
+        which is already full
+    """
+
     def __init__(self, message, value):
         self.message = message
         self.value = value
@@ -57,7 +85,11 @@ class MaxStudError(Exception):
     def __str__(self):
         return f'{self.message}, {self.value}'
 
+
 class StudInGroupError(Exception):
+    """
+    This exception is raised when you try to add student who is already in group
+    """
 
     def __init__(self, message, student):
         self.message = message
@@ -66,24 +98,27 @@ class StudInGroupError(Exception):
     def __str__(self):
         return f'{self.message}, {self.student}'
 
+if __name__ == '__main__':
 
-group = Group('Python', 3)
+    logger.warning('Started loggin to console and file')
+    group = Group('Python', 3)
 
-stud_1 = Student('Kostya', 'Osypenko', 33)
-stud_2 = Student('Mykola', 'Slobodyanik', 32)
-stud_3 = Student('Liza', 'Bernich', 25)
-stud_4 = Student('Serhiy', 'Voevoda', 42)
+    stud_1 = Student('Kostya', 'Osypenko', 33)
+    stud_2 = Student('Mykola', 'Slobodyanik', 32)
+    stud_3 = Student('Liza', 'Bernich', 25)
+    stud_4 = Student('Serhiy', 'Voevoda', 42)
 
-group.add_visitor(stud_1)
-group.add_visitor(stud_2)
-# group.add_visitor(stud_3)
-# group.add_visitor(stud_4)
+    group.add_visitor(stud_1)
+    group.add_visitor(stud_2)
+    group.add_visitor(stud_3)
+    # group.add_visitor(stud_4)
 
-# group.remove_visitor(stud_4)
-# group.remove_visitor(stud_3)
+    # group.remove_visitor(stud_4)
+    # group.remove_visitor(stud_3)
 
-print(group)
-res = group.find_visitor('Bernich')
-for item in res:
-    print(f'Search result: {item}')
+    print(group)
+    res = group.find_visitor('Bernich')
+    for item in res:
+        print(f'Search result: {item}')
 
+    logger.warning('Logging finished')
